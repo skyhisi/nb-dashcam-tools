814c888dc96ffb0b1da3d50619bfe3d0238da0cf
Silas Parker <skyhisi@gmail.com>
2021-05-03 11:14:34 +0100

    Package action fixs to tarball and Docker image

--- .github/docker/rpmpkg/entrypoint.sh
+++ .github/docker/rpmpkg/entrypoint.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 set -ex
+SRC=$(<${{runner.temp}}/filename.txt)
+VER=$(tar -xf "/mnt/$SRC" --strip-components=1 --no-anchored -O version.txt | cut -c2- | grep -Eo '[0-9]+\.[0-9]+\.[0-9]+')
 rpmdev-setuptree
 rpmbuild -D "version $VER" -ta "/mnt/$SRC"
 cp -v "$(rpm --eval "%{_rpmdir}/%{_arch}")/nb-dashcam-tools-$VER-"*.rpm /mnt
